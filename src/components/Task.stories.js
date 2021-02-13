7ba99525337e3981c4d4116d9a00c19c614250cc
Annie Huang <annie.hn@gmail.com>
2021-02-13 17:26:02 +1100

    Add comment to explain Template.bind({}) and .args

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -9,6 +9,12 @@ export default {
 
 const Template = args => <Task {...args} />;
 
+/* Template.bind({}) is a standard JavaScript technique for making a copy of a function.
+   We use this technique to allow each exported story to set its own properties, but use the same implementation.
+
+   Arguments or args for short, allow us to live edit our components with the controls addon without restarting Storybook.
+   Once an args value changes so does the component.
+*/
 export const Default = Template.bind({});
 Default.args = {
   task: {
