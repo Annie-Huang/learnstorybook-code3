e0299c4b2a46b90b50736fbccbdd0f74355b2e80
Annie Huang <annie.hn@gmail.com>
2021-04-05 16:56:00 +1000

    Update App.js to call TaskList.js file. Remove App.test.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,4 +23,22 @@ function App() {
   );
 }
 
+export default App;
+*/
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import TaskList from './components/TaskList';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <TaskList />
+      </Provider>
+    );
+  }
+}
 export default App;
