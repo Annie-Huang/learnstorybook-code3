61dcc65e0ed135513680ef0dd2965b6fad120515
Annie Huang <annie.hn@gmail.com>
2021-04-05 22:01:05 +1000

    Configure @storybook/addon-a11y in main.js. Add Example for Button.stories.js

---
+++ src/components/Button.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { storiesOf, addDecorator, addParameters } from '@storybook/react';
+
+export default {
+  title: 'button',
+  parameters: {
+    a11y: {
+      // optional selector which element to inspect
+      element: '#root',
+      // axe-core configurationOptions (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1)
+      config: {},
+      // axe-core optionsParameter (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter)
+      options: {},
+      // optional flag to prevent the automatic check
+      manual: true,
+    },
+  },
+};
+
+export const accessible = () => <button>Accessible button</button>;
+
+export const inaccessible = () => (
+  <button style={{ backgroundColor: 'red', color: 'darkRed' }}>Inaccessible button</button>
+);
