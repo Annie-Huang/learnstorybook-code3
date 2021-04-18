9ee2161f27da5c66261e0f0892d708652cd366ca
Annie Huang <annie.hn@gmail.com>
2021-04-18 22:48:26 +1000

    Configure coverage test.

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -1,8 +1,20 @@
 import {Button} from '../stories/Button.js';
 import {render, screen} from '@testing-library/react';
+import {Primary} from '../stories/Button.stories';
 
 test('should render', () => {
   render(<Button label='Click Me!' />);
   // expect(screen.getByRole('button')).toHaveTextContent('click');
   expect(screen.getByRole('button')).toHaveTextContent(/click/i); // regex anything contains the word 'click'
+});
+
+test('should render1', () => {
+  render(<Primary {...Primary.args} />);
+  /* this is because
+    Primary.args = {
+      primary: true,
+      label: 'Button',  <--------------
+    };
+   */
+  expect(screen.getByRole('button')).toHaveTextContent(/button/i);
 });
